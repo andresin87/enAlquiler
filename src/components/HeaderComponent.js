47b39adedf768220ac8670e4c0b5803c024f9ea3
Andrés Lucas Enciso <andresin87@gmail.com>
2017-01-18 16:41:35 +0100

    ADD header component

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import { grey100 } from 'material-ui/styles/colors'
+
+require('styles//Header.scss');
+
+let enAlquilerSVG = require('../../assets/enAlquiler.svg');
+
+class HeaderComponent extends React.Component {
+  render() {
+    return (
+      <AppBar
+        style={{
+          backgroundColor: grey100,
+        }}
+        title={
+          <img
+            src={enAlquilerSVG}
+            style={{
+              height: 48,
+              marginTop: 10,
+              marginLeft: 30,
+            }}
+          />
+        }
+        showMenuIconButton={false}
+      />
+    );
+  }
+}
+
+HeaderComponent.displayName = 'HeaderComponent';
+
+// Uncomment properties you need
+// HeaderComponent.propTypes = {};
+// HeaderComponent.defaultProps = {};
+
+export default HeaderComponent;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,29 @@ require('normalize.css/normalize.css');
 require('styles/App.scss');
 
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Header from './HeaderComponent';
+
+injectTapEventPlugin();
 
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <Header />
+          <div className="index">
+            <img src={yeomanImage} alt="Yeoman Generator" />
+            <div style={{}}>
+              <RaisedButton label="OK" primary={true} />
+            </div>
+          </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
